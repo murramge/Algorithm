3287ce7008277df1383da14baa07969c18c0483d
00-au <dmdgi37@naver.com>
2022-06-05 04:02:35 +0900

    Create 이진수출력_재귀.java
    이진수출력_재귀

---
+++ coding_test_algorithum/DFS_BFS_기초/이진수출력_재귀.java
@@ -0,0 +1,15 @@
+package DFS_BFS_기초;
+
+public class 이진수출력_재귀 {
+	public void DFS(int n) {
+		if(n==0) return;
+		else {
+			DFS(n/2);
+			System.out.print(n%2+ " ");
+		}
+	}
+	public static void main(String[] args) {
+		이진수출력_재귀 T = new 이진수출력_재귀();
+		T.DFS(11);
+	}
+}

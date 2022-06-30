3a8838d05b0cadfbbc9ce6fa9966c925b53388b4
00-au <dmdgi37@naver.com>
2022-06-30 17:52:55 +0900

    팩토리얼,피보나치
    팩토리얼,피보나치

---
+++ coding_test_algorithum/DFS_BFS_기초/피보나치수열.java
@@ -0,0 +1,22 @@
+package DFS_BFS_기초;
+
+public class 피보나치수열 {
+	static int[] fibo;
+	public int DFS(int n) {
+		if(fibo[n]>0) {
+			return fibo[n];
+		}
+		if(n==1) return fibo[n]=1;
+		else if(n==2) return fibo[n]=1;
+		else return fibo[n]=DFS(n-2)+DFS(n-1);
+	}
+	public static void main(String[] args) {
+		피보나치수열 T = new 피보나치수열();
+		int n=45;
+		fibo = new int[n+1];
+		T.DFS(n);
+		for(int i=1; i<=n; i++) {
+		System.out.print(fibo[i]+" ");
+		}
+	}	
+}
